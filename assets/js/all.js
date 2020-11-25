6b30b668bc617c7318f6e2fa23abb7233b1fb63d
SuPan <joelopan@gmail.com>
2020-11-26 01:19:30 +0800

    Update 2020-11-25T17:19:26.501Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,7 +1,11 @@
 "use strict";
 
+var url = window.location.pathname;
 console.log('Hello!');
 $(document).ready(function () {
   console.log('HesSchool Hello!');
+  $('.nav-item').find('a').each(function () {
+    $(this).toggleClass('nav-link--active', $(this).attr('href') == url.split('/')[1]);
+  });
 });
 //# sourceMappingURL=all.js.map
