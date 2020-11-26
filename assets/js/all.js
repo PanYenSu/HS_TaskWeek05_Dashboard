ac639f141760941d31e4bedbc8152aa6396a8204
SuPan <joelopan@gmail.com>
2020-11-27 01:32:13 +0800

    Update 2020-11-26T17:32:10.613Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -7,5 +7,8 @@ $(document).ready(function () {
   $('.nav-item').find('a').each(function () {
     $(this).toggleClass('nav-link--active', $(this).attr('href') == url.split('/')[1]);
   });
+  $('.list-group-item-link').click(function () {
+    $(this).addClass("list-group-item-action");
+  });
 });
 //# sourceMappingURL=all.js.map
