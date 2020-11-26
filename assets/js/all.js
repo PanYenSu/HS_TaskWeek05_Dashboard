15f8bd57a83943754ec7a0c5617eb355f5309883
SuPan <joelopan@gmail.com>
2020-11-27 01:39:08 +0800

    Update 2020-11-26T17:39:06.770Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -6,9 +6,8 @@ $(document).ready(function () {
   console.log('HesSchool Hello!');
   $('.nav-item').find('a').each(function () {
     $(this).toggleClass('nav-link--active', $(this).attr('href') == url.split('/')[1]);
-  });
-  $('.list-group-item-link').click(function () {
-    $(this).addClass("list-group-item-action");
-  });
+  }); // $('.list-group-item-link').click(function(){
+  //   $(this).addClass("list-group-item-action");
+  // });
 });
 //# sourceMappingURL=all.js.map
