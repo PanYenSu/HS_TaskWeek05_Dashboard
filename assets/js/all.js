46ff2bcc38de1c0375364f88ddb36d5f8d82140d
SuPan <joelopan@gmail.com>
2020-12-09 09:19:01 +0800

    Update 2020-12-09T01:18:59.562Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -17,5 +17,14 @@ $(document).ready(function () {
     $('.reply').removeClass('d-none');
     $('.assignment-footer').removeClass('d-none');
   });
+  $('.btn-verifie').click(function () {
+    if ($('.verifie-hide').hasClass('d-none')) {
+      $('.verifie-show').addClass('d-none');
+      $('.verifie-hide').removeClass('d-none');
+    } else {
+      $('.verifie-hide').addClass('d-none');
+      $('.verifie-show').removeClass('d-none');
+    }
+  });
 });
 //# sourceMappingURL=all.js.map
